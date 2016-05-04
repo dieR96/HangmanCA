b86847f4f6e8edf0cb9a7995096bdf18ce7078f5
Darren Reid <darrenr96@gmail.com>
2016-05-04 22:43:23 +0100

    Added Pseudocode and rules/welcome strings on load of main

---
+++ hangman.hs
@@ -0,0 +1,19 @@
+-- Hangman Fucnctional Programming Final CA --
+--PseudoCode--
+--Print welcome to game pick random word or enter in own word, option to display rules--
+main = do
+        putStrLn "Welcome to my Hangman game!!"
+		putStrLn "Rules: 1. Play individually or in groups."
+		putStrLn "       2. Have the student select a letter of the alphabet."
+		putStrLn "       3. If letter guessed is in word display that letter."
+		putStrLn "       4. If letter guessed is not in word display an error."
+		putStrLn "       5. Game goes until guesses run out or the user wins."
+--When word is chosen it is displayed like this ---------(for loop) blanks and if a word is guessed right, - turns to letter--
+
+--Set amount of guesses allowed, if letter guessed is wrong turns -1--
+
+--Prompt user to enter guesses, if they complete word display winning message--
+
+
+
+
