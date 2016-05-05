a3a1a98755bd3cb4c1a5870aaf2ad7664c3585c9
Darren Reid <darrenr96@gmail.com>
2016-05-05 02:43:03 +0100

    Fixed output of instructions to fix errors

--- hangman.hs
+++ hangman.hs
@@ -2,18 +2,13 @@
 --PseudoCode--
 --Print welcome to game pick random word or enter in own word, option to display rules--
 main = do
-        putStrLn "Welcome to my Hangman game!!"
-		putStrLn "Rules: 1. Play individually or in groups."
-		putStrLn "       2. Have the student select a letter of the alphabet."
-		putStrLn "       3. If letter guessed is in word display that letter."
-		putStrLn "       4. If letter guessed is not in word display an error."
-		putStrLn "       5. Game goes until guesses run out or the user wins."
+    putStrLn $ "Welcome to my Hangman game!!"
+    putStrLn $ "Instructions: 1. Enter word to guess off and begin guessing away!"
+    putStrLn $ "              2. The game will end if you win or if you run out of guesses"
+    putStrLn $ "Please enter a word to guess off!"
+	
 --When word is chosen it is displayed like this ---------(for loop) blanks and if a word is guessed right, - turns to letter--
 
 --Set amount of guesses allowed, if letter guessed is wrong turns -1--
 
 --Prompt user to enter guesses, if they complete word display winning message--
-
-
-
-
